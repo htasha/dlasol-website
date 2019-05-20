ee0df4ab2a126acce24286bae4607c3874c25d71
htasha <ahsath@gmail.com>
2019-05-20 09:28:40 -0500

    Make components globally so gsap can work

--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ import 'iview/dist/styles/iview.css';
 import './assets/css/global.css';
 import 'typeface-eb-garamond';
 import 'typeface-josefin-sans';
+import { TweenLite, ScrollToPlugin } from "gsap/all";
 import TheFooter from '~/layouts/TheFooter.vue'
+import Courses from '~/components/Courses.vue'
+import Services from '~/components/Services.vue'
+import Portfolio from '~/components/Portfolio.vue'
+import Bio from '~/components/Bio.vue'
+
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
@@ -19,4 +25,8 @@ export default function (Vue, { router, head, isClient }) {
   Vue.component('Menu', Menu);
   Vue.component('Sider', Sider);
   Vue.component('TheFooter', TheFooter)
+  Vue.component('Courses', Courses)
+  Vue.component('Services', Services)
+  Vue.component('Portfolio', Portfolio)
+  Vue.component('Bio', Bio)
 }
