4eb1267be64a83384b026294f1fbf9b194962c04
htasha <ahsath@gmail.com>
2019-05-20 19:34:21 -0500

    Updated

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Dlasol',
+  siteUrl: 'https://dlasol.com',
+  siteDescription: 'We are a creative agency based on Plant City, Florida, that provides courses and services in a variety of subjects, from clothing design to english classes, and more.',
+  titleTemplate: 'Dlasol - %s'
 }
